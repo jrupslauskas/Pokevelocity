e9b02901a1a951a3d34df63e2e34006a5d0a0c4e
Nick Wessing <nwessing3@gmail.com>
2025-11-18 18:15:30 -0600

    Encounter Animation
    Use webgl to implement an encounter animation.
    Mimics the horizontal strips enounter animation from gen1 pokemon.

---
+++ app/javascript/encounter_animation.js
@@ -0,0 +1,131 @@
+
+const VERTEX_SHADER_SOURCE = `#version 300 es
+out highp vec2 vUv;
+
+void main() {
+  vec2 vertices[6];
+  vertices[0] = vec2(0.0, 0.0);
+  vertices[1] = vec2(1.0, 0.0);
+  vertices[2] = vec2(1.0, 1.0);
+  vertices[3] = vec2(1.0, 1.0);
+  vertices[4] = vec2(0.0, 1.0);
+  vertices[5] = vec2(0.0, 0.0);
+  vec2 uv = vertices[gl_VertexID];
+
+  gl_Position = vec4(uv * 2.0 - vec2(1.0), 0.0, 1.0);
+  vUv = uv;
+}
+`;
+
+const FRAGMENT_SHADER_SOURCE = `#version 300 es
+uniform highp float uTime;
+uniform sampler2D uSampler;
+in highp vec2 vUv;
+out highp vec4 fragColor;
+
+void main() {
+  highp vec4 color = texture(uSampler, vUv);
+  fragColor = vec4(vec3(0.0), color.r > uTime ? 0.0 : 1.0);
+}
+`;
+
+const ANIMATION_LENGTH = 2000.0;
+const BLACKOUT_LENGTH = 500.0;
+
+document.addEventListener('turbo:load', function () {
+
+  const poke = {
+    gl: null,
+  };
+
+  function initGl() {
+    const canvas = document.getElementById('animation-overlay');
+    poke.gl = canvas.getContext('webgl2');
+    let gl = poke.gl;
+
+    poke.vs = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(poke.vs, VERTEX_SHADER_SOURCE);
+    gl.compileShader(poke.vs);
+
+    poke.fs = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(poke.fs, FRAGMENT_SHADER_SOURCE);
+    gl.compileShader(poke.fs);
+
+    if (!gl.getShaderParameter(poke.vs, gl.COMPILE_STATUS)) {
+      console.error(`${gl.getShaderInfoLog(poke.vs)}`);
+    }
+
+    if (!gl.getShaderParameter(poke.fs, gl.COMPILE_STATUS)) {
+      console.error(`${gl.getShaderInfoLog(poke.fs)}`);
+    }
+
+    poke.program = gl.createProgram();
+    gl.attachShader(poke.program, poke.vs);
+    gl.attachShader(poke.program, poke.fs);
+    gl.linkProgram(poke.program);
+
+    if (!gl.getProgramParameter(poke.program, gl.LINK_STATUS)) {
+      console.error(`${gl.getProgramInfoLog(poke.program)}`);
+    }
+  }
+
+  initGl();
+  poke.texture = loadImage(window.assetPaths.horizontalStripes);
+
+  function loadImage(imageUrl) {
+    const gl = poke.gl;
+    const texture = gl.createTexture();
+
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    // placeholder data while image is loading
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, new Uint8Array([0, 0, 0, 255]));
+
+
+    const image = new Image();
+    image.onload = function () {
+      gl.bindTexture(gl.TEXTURE_2D, texture);
+      gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+      gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+      gl.generateMipmap(gl.TEXTURE_2D);
+    };
+    image.src = imageUrl;
+
+    return texture;
+  }
+
+  function startAnimation(timestamp) {
+    let gl = poke.gl;
+    gl.clearColor(1.0, 1.0, 1.0, 0.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.useProgram(poke.program);
+    let dt = (timestamp - poke.animationStartTime) / ANIMATION_LENGTH;
+    gl.uniform1f(gl.getUniformLocation(poke.program, 'uTime'), dt);
+
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE2D, poke.texture);
+    gl.uniform1i(gl.getUniformLocation(poke.program, 'uSampler'), 0);
+
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 6);
+
+    requestAnimationFrame(startAnimation);
+  }
+
+  document.querySelectorAll('button.pokeball-card').forEach(function (button) {
+    button.addEventListener('click', function(event) {
+      // Stops the button from posting the form
+      event.preventDefault();
+
+      document.getElementById("animation-overlay").classList.remove('hidden');
+      poke.animationStartTime = document.timeline.currentTime;
+      requestAnimationFrame(startAnimation);
+
+      setTimeout(function () {
+        document.getElementById("animation-overlay").classList.add('hidden');
+        // Finally posts the form once the animation and blackout period are complete
+        button.form.submit();
+      }, ANIMATION_LENGTH + BLACKOUT_LENGTH);
+    });
+  });
+});
